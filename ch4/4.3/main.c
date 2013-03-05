f28a031259d4910a4fdfb20d557fcc449ee82065
JohnCheng <johncheng@linpus.com>
2013-03-05 08:36:33 +0800

    complete ex 4.3

--- ch4/4.3/main.c
+++ ch4/4.3/main.c
@@ -37,6 +37,13 @@ int main(int argc, char **argv)
 	    else
 		printf("Error: zero divisor\n");
 	    break;
+	case '%':   /* ex 4.3: add modulus operator */
+	    op2 = pop();
+	    if (op2 != 0.0)
+		push((int)pop() % (int) op2);
+	    else
+		printf("Error: zero divisor\n");
+	    break;
 	case '\n':
 	    printf("\t%.8g\n", pop());
 	    break;
